517209553e9244ad4d87396d1057cc53ef478237
vongocquan <quanv4297@gmail.com>
2020-10-29 23:54:27 +0700

    list customer

---
+++ ParkingWebAppFE/src/app/customer/customer.ts
@@ -0,0 +1,14 @@
+export class Customer {
+  id: number;
+  nameCustomer: string;
+  birthday: string;
+  email: string;
+  phone: string;
+  gender: string;
+  idCard: string;
+  address: string;
+  cars: number[];
+
+  constructor() {
+  }
+}
