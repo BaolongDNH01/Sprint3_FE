a1dde0511dedf4a7c0dff1469c757af9dc7a5753
chaudang <chaudang1595@gmail.com>
2020-11-02 14:59:13 +0700

    Chau update statistic

--- ParkingWebAppFE/src/app/car/car.service.ts
+++ ParkingWebAppFE/src/app/car/car.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Car} from './car';
@@ -7,9 +7,20 @@ import {Car} from './car';
   providedIn: 'root'
 })
 export class CarService {
-  URL_API = 'http://localhost:8080/'
-  constructor(private httpClient: HttpClient) { }
-  findCarByCustomer(id: number): Observable<Car[]>{
+  URL_API = 'http://localhost:8080/';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  findCarByCustomer(id: number): Observable<Car[]> {
     return this.httpClient.get<Car[]>(this.URL_API + 'get-car-by-customer/' + id);
+  }
+
+  findAllCar(): Observable<Car[]> {
+    return this.httpClient.get<Car[]>(this.URL_API + 'all-car');
+  }
+// Chau
+  findAllCarByType(type: string): Observable<Car[]>{
+    return this.httpClient.get<Car[]>(this.URL_API + 'getAllCarByType/' + type);
   }
 }
--- ParkingWebAppFE/src/app/statistical/statistical.component.ts
+++ ParkingWebAppFE/src/app/statistical/statistical.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Chart} from 'chart.js';
 import {CustomerService} from '../customer/customer.service';
+import {CarService} from '../car/car.service';
+import {TicketService} from '../ticket/services/ticket.service';
+import {Car} from '../car/car';
 
 @Component({
   selector: 'app-statistical',
@@ -12,11 +15,27 @@ export class StatisticalComponent implements OnInit {
   PieChart = [];
   a = 22;
   countCustomer: number;
-  countCar: number
+  countCar: number;
+  countTicket: number;
+  carList: Car[];
+  typeCar = [];
+  color = [];
+  countTypeCar = [];
+  countTypeCar1: number;
 
   constructor(
-    private customerService: CustomerService
+    private customerService: CustomerService,
+    private ticketService: TicketService,
+    private carService: CarService
   ) {
+  }
+
+  ngOnInit(): void {
+    this.findAllCustomer();
+    this.findAllTicket();
+    this.findAllCar();
+    console.log(this.typeCar);
+    console.log(this.countTypeCar);
   }
 
   findAllCustomer(): void {
@@ -27,36 +46,83 @@ export class StatisticalComponent implements OnInit {
     );
   }
 
-  ngOnInit(): void {
-    this.findAllCustomer();
-    // @ts-ignore
-    this.PieChart = new Chart('pieChart', {
-      type: 'pie',
-      data: {
-        labels: ['Blue', 'Green', 'Pink'],
-        datasets: [{
-          label: 'Vote Now',
-          data: [101, this.a, 103],
-          backgroundColor: [
-            'rgba(40,23,244,0.9)',
-            'rgba(192,255,0,0.9)',
-            'rgba(239,23,240,0.9)',
-          ],
-        }]
-      },
-      options: {
-        title: {
-          text: 'Bar Chart',
-          display: true
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
+  findAllTicket(): void {
+    this.ticketService.getAllTicket().subscribe(
+      list => {
+        this.countTicket = list.length;
       }
-    });
+    );
   }
+
+  findAllCar(): void {
+    this.carService.findAllCar().subscribe(
+      list => {
+        this.countCar = list.length;
+        this.carList = list;
+
+        for (let i = 0; i < this.carList.length; i++) {
+          if (this.typeCar.includes(list[i].type)) {
+          } else {
+            this.typeCar.push(list[i].type);
+            this.carService.findAllCarByType(list[i].type).subscribe(
+              carByType => {
+                console.log(carByType.length);
+                this.countTypeCar.push(carByType.length);
+              }, error => {
+
+              },
+              () => {
+              // @ts-ignore
+                this.PieChart = new Chart('pieChart', {
+                  type: 'pie',
+                  data: {
+                    labels: this.typeCar,
+                    datasets: [{
+                      label: 'Vote Now',
+                      data: this.countTypeCar,
+                      backgroundColor: this.color,
+                    }]
+                  },
+                  options: {
+                    title: {
+                      text: 'Bar Chart',
+                      display: true
+                    },
+                    scales: {
+                      yAxes: [{
+                        ticks: {
+                          beginAtZero: true
+                        }
+                      }]
+                    }
+                  }
+                });
+
+              }
+            );
+          }
+
+          this.color.push(this.getRandomColor());
+        }
+
+
+      }, error => {
+
+      }, () => {
+        console.log(this.countTypeCar);
+
+      }
+    );
+  }
+
+
+  getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
 }
