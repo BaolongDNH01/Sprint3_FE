7fe54981f7627a526a6a50c5a953fb7b260c986c
Thien Pham <phamanhthien1907@gmail.com>
2020-11-03 10:42:29 +0700

    Thien: Remake routing

--- ParkingWebAppFE/src/app/app-routing.module.ts
+++ ParkingWebAppFE/src/app/app-routing.module.ts
@@ -5,22 +5,51 @@ import {HomepageComponent} from './homepage/homepage.component';
 import {ParkingManagementComponent} from './parking-management/parking-management.component';
 import {AuthLoginComponent} from './login/auth-login/auth-login.component';
 import {ListUserComponent} from './user/list-user/list-user.component';
+import { AddUserComponent } from './user/add-user/add-user.component';
+import { DetailUserComponent } from './user/detail-user/detail-user.component';
 
 
 export const routes: Routes = [
   {
-    path: 'ticket',
-    loadChildren: () => import('./ticket/ticket.module').then(m => m.TicketModule)
-  },
-  {
-    path: '', children: [
-
-      {path: '', component: AuthLoginComponent},
-      {path: 'home', component: HomepageComponent},
-
+    path: '', 
+    component: AuthLoginComponent,
+    children: [
+      {
+        path: 'home', 
+        component: HomepageComponent},
+      {
+        path: 'parking', 
+        component: ParkingManagementComponent
+      },
+      {
+        path: 'ticket',
+        loadChildren: () => import('./ticket/ticket.module').then(m => m.TicketModule)
+      },
+      {
+        path: 'add-user', 
+        component: AddUserComponent
+      },
+      {
+        path: 'user/:id', 
+        component: DetailUserComponent
+      },
     ]
   }];
 
+  // Routing của PHƯƠNG -_-
+  // {
+  //   path: '', children: [
+  //     {path: 'login', component: AuthLoginComponent},
+  //     {
+  //       path: '', component: HomepageComponent, children: [
+  //         {path: 'ticket', component: ParkingManagementComponent},
+  //         {path: 'add-user', component: AddUserComponent},
+  //         {path: 'user/:id', component: DetailUserComponent},
+  //       ]
+  //     },
+  //   ]
+  // }];
+  
 
 @NgModule({
   declarations: [],
