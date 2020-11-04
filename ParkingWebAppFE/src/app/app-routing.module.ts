9cfde75ce89b5bcf2eb9911aa15f479fa98b05d3
tocdovodoi <thanhtungspt@gmail.com>
2020-11-05 03:01:31 +0700

    Tung2
    1. Thêm router info, add, list-customer
    2. Đổ thông tin ra info-customer

--- ParkingWebAppFE/src/app/app-routing.module.ts
+++ ParkingWebAppFE/src/app/app-routing.module.ts
@@ -7,6 +7,9 @@ import { AuthLoginComponent } from './login/auth-login/auth-login.component';
 import { ListUserComponent } from './user/list-user/list-user.component';
 import { AddUserComponent } from './user/add-user/add-user.component';
 import { DetailUserComponent } from './user/detail-user/detail-user.component';
+import {ListCustomerComponent} from './customer/list-customer/list-customer.component';
+import {AddCustomerComponent} from './customer/add-customer/add-customer.component';
+import {InfoCustomerComponent} from './customer/info-customer/info-customer.component';
 
 
 export const routes: Routes = [
@@ -22,6 +25,9 @@ export const routes: Routes = [
           {path: 'add-user', component: AddUserComponent},
           {path: 'user/:id', component: DetailUserComponent},
           {path: 'list-user', component: ListUserComponent},
+          {path: 'list-customer', component: ListCustomerComponent},
+          {path: 'add-customer', component: AddCustomerComponent},
+          {path: 'info-customer/:id', component: InfoCustomerComponent}
 
         ]
       },
