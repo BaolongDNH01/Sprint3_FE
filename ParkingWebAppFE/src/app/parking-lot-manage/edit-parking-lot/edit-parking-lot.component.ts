282da58459c2c89c7863f9b35a56afbe87b48118
doquockhanh <khanhquocdo.qt@gmail.com>
2020-11-04 10:05:10 +0700

    khanh create component add and edit parking-lot

---
+++ ParkingWebAppFE/src/app/parking-lot-manage/edit-parking-lot/edit-parking-lot.component.ts
@@ -0,0 +1,74 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ParkingLot} from '../entity/parking-lot';
+import {Floor} from '../entity/floor';
+import {Zone} from '../entity/zone';
+import {ParkingLotService} from '../service/parking-lot.service';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+
+@Component({
+  selector: 'app-edit-parking-lot',
+  templateUrl: './edit-parking-lot.component.html',
+  styleUrls: ['./edit-parking-lot.component.css']
+})
+export class EditParkingLotComponent implements OnInit {
+  editParkingLotForm: FormGroup;
+  parkingLot: ParkingLot;
+  listFloor: Floor[];
+  listZoneApi: Zone[];
+  listZoneShow: Zone[];
+  id: number;
+
+  constructor(private formBuilder: FormBuilder,
+              private parkingLotService: ParkingLotService,
+              private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params: ParamMap) => {
+      // tslint:disable-next-line:radix
+      this.id = parseInt(params.get('id'));
+      this.id = 1;
+      this.parkingLotService.getParkingLotById(this.id).subscribe(
+        par => {
+          this.parkingLot = par;
+          this.form();
+        }
+      );
+    });
+  }
+
+  form(): void {
+    this.parkingLotService.getAllFloor().subscribe(
+      list => this.listFloor = list,
+    );
+
+    this.parkingLotService.getAllZoneByFloor(0).subscribe(
+      list => {
+        this.listZoneApi = list;
+        this.switchFloor(1);
+      }
+    );
+
+    this.editParkingLotForm = this.formBuilder.group({
+      id: [this.parkingLot.id],
+      idFloor: [this.parkingLot.idFloor, Validators.required],
+      idZone: [this.parkingLot.idZone, Validators.required],
+      status: [this.parkingLot.status, Validators.required]
+    });
+  }
+
+  editParkingLot(): void {
+    this.parkingLot = this.editParkingLotForm.value;
+    this.parkingLotService.editParkingLot(this.parkingLot).subscribe();
+  }
+
+  switchFloor(floor: number): void {
+    this.listZoneShow = this.listZoneApi.filter(zone => {
+      //  cần để ==
+      if (zone.idFloor == floor) {
+        return zone;
+      }
+    });
+  }
+}
--- ParkingWebAppFE/src/app/parking-lot-manage/parking-lot-manage.module.ts
+++ ParkingWebAppFE/src/app/parking-lot-manage/parking-lot-manage.module.ts
@@ -3,18 +3,24 @@ import { CommonModule } from '@angular/common';
 import { ParkingMapComponent } from './parking-map/parking-map.component';
 import { ListParkingLotComponent } from './list-parking-lot/list-parking-lot.component';
 import {NgxPaginationModule} from 'ngx-pagination';
+import { AddParkingLotComponent } from './add-parking-lot/add-parking-lot.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { EditParkingLotComponent } from './edit-parking-lot/edit-parking-lot.component';
 
 
 
 @NgModule({
-  declarations: [ParkingMapComponent, ListParkingLotComponent],
-    exports: [
-        ParkingMapComponent,
-        ListParkingLotComponent
-    ],
-  imports: [
-    CommonModule,
-    NgxPaginationModule
-  ]
+  declarations: [ParkingMapComponent, ListParkingLotComponent, AddParkingLotComponent, EditParkingLotComponent],
+  exports: [
+    ParkingMapComponent,
+    ListParkingLotComponent,
+    AddParkingLotComponent,
+    EditParkingLotComponent
+  ],
+    imports: [
+        CommonModule,
+        NgxPaginationModule,
+        ReactiveFormsModule
+    ]
 })
 export class ParkingLotManageModule { }
