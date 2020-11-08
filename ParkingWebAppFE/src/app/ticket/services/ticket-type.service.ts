51319350d107ebed6bdbf4a2e3dc90ac04e3908d
Thien Pham <phamanhthien1907@gmail.com>
2020-11-08 15:32:59 +0700

    Thien: Finish create ticket form, config get parkinglot

--- ParkingWebAppFE/src/app/ticket/services/ticket-type.service.ts
+++ ParkingWebAppFE/src/app/ticket/services/ticket-type.service.ts
@@ -1,9 +1,17 @@
+import { TicketType } from './../models/TicketType';
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class TicketTypeService {
 
-  constructor() { }
+  private TICKET_API = 'http://localhost:8080/ticket-type';
+
+  constructor(private http: HttpClient) { }
+
+  getAllTicketType(): Observable<TicketType[]> {
+    return this.http.get<TicketType[]>(`${this.TICKET_API}/all`);
+  }
 }
