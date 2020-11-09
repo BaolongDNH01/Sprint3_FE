4d5ce911e79dcfdb5f50e6738389d9cf1e794136
Thien Pham <phamanhthien1907@gmail.com>
2020-11-09 08:25:21 +0700

    Thien: Finish create ticket

--- ParkingWebAppFE/src/app/customer/add-customer/add-customer.component.ts
+++ ParkingWebAppFE/src/app/customer/add-customer/add-customer.component.ts
@@ -1,10 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import {Customer} from '../customer';
-import {Router} from '@angular/router';
-import {CustomerService} from '../customer.service';
-import {Car} from '../../car/car';
-import {CarService} from '../../car/car.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Customer } from '../customer';
+import { Router } from '@angular/router';
+import { CustomerService } from '../customer.service';
+import { Car } from '../../car/car';
+import { CarService } from '../../car/car.service';
 
 @Component({
   selector: 'app-add-customer',
@@ -72,6 +72,9 @@ export class AddCustomerComponent implements OnInit {
     this.customerService.createCustomer(this.customer).subscribe(
       next => {
         console.log('Create process!');
+
+        // Thien lưu tên khách hàng và biển số xe để tạo vé
+        localStorage.setItem('customerName', this.customer.nameCustomer);
       }, error => {
         console.log('Create failed!');
       }, () => {
@@ -98,7 +101,7 @@ export class AddCustomerComponent implements OnInit {
         );
       }
     );
-    this.router.navigateByUrl('/list-customer');
+    this.router.navigateByUrl('ticket/list');
   }
 
   submitAll(): void {
@@ -110,6 +113,7 @@ export class AddCustomerComponent implements OnInit {
     this.car.carId = this.carId;
     this.carService.addCar(this.car).subscribe(
       list => {
+        localStorage.setItem('license', this.car.license);
       }, error => {
       }, () => {
       }
